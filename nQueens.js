680b7b29add14236fc2004f4add16fc6db1c4151
Mario <mpontice@gmail.com>
2014-07-31 12:28:17 -0700

    Set up basic tests

--- nQueens.js
+++
@@ -1,6 +0,0 @@
-/*! nQueens v0.0.0 - MIT license */
-'use strict';
-
-var nQueens = function () {
-  // your code goes here
-}
--- spec/nQueens.js
+++ spec/nQueens.js
@@ -1,10 +1,29 @@
 /* global nQueens, describe, it, expect, should */
 
-describe('nQueens()', function () {
+describe('nQueensBit()', function () {
   'use strict';
 
   it('exists', function () {
-    expect(nQueens).to.be.a('function');
+    expect(nQueensBit).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
+
+describe('nQueensHash()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(nQueensHash).to.be.a('function');
 
   });
 
