e02459c8c455d66b5152eac23564992033e1b485
Mario <mpontice@gmail.com>
2014-07-31 15:16:13 -0700

    Implement useful bitwise and hash-table tests

--- spec/nQueens.js
+++ spec/nQueens.js
@@ -8,15 +8,26 @@ describe('nQueensBit()', function () {
 
   });
 
-  it('does something', function () {
-    expect(true).to.equal(false);
+  it('should solve for n = 0', function () {
+    expect(nQueensBit(0)).to.equal(1);
   });
 
-  it('does something else', function () {
-    expect(true).to.equal(false);
+  it('should solve for n = 2', function () {
+    expect(nQueensBit(2)).to.equal(0);
   });
 
-  // Add more assertions here
+  it('should solve for n = 4', function () {
+    expect(nQueensBit(4)).to.equal(2);
+  });
+
+  it('should solve for n = 8', function () {
+    expect(nQueensBit(8)).to.equal(92);
+  });
+
+  it('should solve for n = 12', function () {
+    expect(nQueensBit(12)).to.equal(14200);
+  });
+
 });
 
 describe('nQueensHash()', function () {
@@ -27,13 +38,24 @@ describe('nQueensHash()', function () {
 
   });
 
-  it('does something', function () {
-    expect(true).to.equal(false);
+  it('should solve for n = 0', function () {
+    expect(nQueensHash(0)).to.equal(1);
   });
 
-  it('does something else', function () {
-    expect(true).to.equal(false);
+  it('should solve for n = 2', function () {
+    expect(nQueensHash(2)).to.equal(0);
   });
 
-  // Add more assertions here
+  it('should solve for n = 4', function () {
+    expect(nQueensHash(4)).to.equal(2);
+  });
+
+  it('should solve for n = 8', function () {
+    expect(nQueensHash(8)).to.equal(92);
+  });
+
+  it('should solve for n = 12', function () {
+    expect(nQueensHash(12)).to.equal(14200);
+  });
+
 });
